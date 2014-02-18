ed23f705678cdccbe693c751bc075bf9707409dc
Julia Savinkova <julsavinkova@gmail.com>
2014-02-19 00:47:49 +0200

    Initial commit

---
+++ js/octocats.js
@@ -0,0 +1,18 @@
+(function($) {
+    $(function() {
+        $("[data-jcarousel]").each(function() {
+            var el = $(this);
+            el.jcarousel(el.data());
+        });
+
+        $("[data-jcarousel-control]").each(function() {
+            var el = $(this);
+            el.jcarouselControl(el.data());
+        });
+
+        $("[data-jcarousel]").jcarouselAutoscroll({
+            target: "+=1",
+            interval: 1000
+        });
+    });
+})(jQuery);
